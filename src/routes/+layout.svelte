a56f242670738d660d296882b056a0d867cb3f4f
dvemolina <dvemolina@gmail.com>
2024-12-01 19:53:51 +0100

    Added DarkMode selector and ModeWatcher dependency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,12 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
 </script>
 
+<ModeWatcher/>
 <ParaglideJS {i18n}>
 	{@render children()}
 </ParaglideJS>
+
